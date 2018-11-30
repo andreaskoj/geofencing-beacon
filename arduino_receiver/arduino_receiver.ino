fabedebfdb0da19cdd03c825fea1a2586a1a9674
Andreas Koj <kojandreas@gmail.com>
2018-11-30 17:58:04 +0100

    implement broadcasting verticles from EEPROM

--- arduino_receiver/arduino_receiver.ino
+++ arduino_receiver/arduino_receiver.ino
@@ -1,6 +1,5 @@
+// RECEIVER CODE - NANO
 
-
-// RECEIVER CODE - Arduino Nano
     #include <nRF24L01.h>
     #include <ArduinoJson.h>
     #include <RF24.h>
@@ -13,36 +12,9 @@
     byte value;
     const byte address[6] = "00001";
     
-
      void setup() {
-      
       Serial.begin(9600);
       while (!Serial) continue;
-
-      const size_t bufferSize = JSON_ARRAY_SIZE(2) + JSON_OBJECT_SIZE(2) + 2*JSON_OBJECT_SIZE(3);
-      DynamicJsonBuffer jsonBuffer(bufferSize);
-      
-      JsonObject& root = jsonBuffer.createObject();
-      root["name"] = "beacon1";
-      
-      JsonArray& verticles = root.createNestedArray("verticles");
-      
-      JsonObject& verticles_0 = verticles.createNestedObject();
-      verticles_0["number"] = 1;
-      verticles_0["longtitude"] = "6.88";
-      verticles_0["langtitude"] = "5.77";
-      
-      JsonObject& verticles_1 = verticles.createNestedObject();
-      verticles_1["number"] = 1;
-      verticles_1["longtitude"] = "9.88";
-      verticles_1["langtitude"] = "2.77";
-      
-      root.printTo(Serial);
-        
-      //EEPROM.write(addressEeprom, 123);
-      value = EEPROM.read(addressEeprom);
-      Serial.println(EEPROM.length());
-      Serial.println(value, DEC); 
       
       radio.begin();
       radio.openReadingPipe(0, address);
--- arduino_transmitter/arduino_transmitter.ino
+++ arduino_transmitter/arduino_transmitter.ino
@@ -1,10 +1,16 @@
 // TRANSMITTER CODE - UNO
 
+// Mode types; create=0, broadcast=1, update=2, delete=3
+
+//TODO
+// - limit max verticles to store 
+
 // to write structures in eeprom
 #include "EEPROMAnything.h"
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+
 
 // verticle object sizeof = 16 bytes
 struct VerticleEEPROM
@@ -15,100 +21,50 @@ struct VerticleEEPROM
 };
 
 RF24 radio(7, 8); // CE, CSN
-char text[200];
-char text2[200];
-int i = 0;
+
+VerticleEEPROM * verticlesArray;
+
 //broadcast address
 const byte address[6] = "00001";
 const int eepromVerticleSize = 16;
-const int deviceID;
+int deviceID = 0;
 //storing qty of verticies 
 int qtyVerticles;
-VerticleEEPROM v1, v2, v3, v4;
-
+int counter = 0;
+char charBuffer[32];
 void setup() {
-Serial.begin(9600); 
-  v1.verticleNumber = 0;
-  strcpy(v1.x, "1.2233");
-  strcpy(v1.y, "5.1234");
-  
-  v2.verticleNumber = 1;
-  strcpy(v2.x, "6.5556");
-  strcpy(v2.y, "9.7785");
+  Serial.begin(9600);
+  while (!Serial) continue;
 
-  v3.verticleNumber = 2;
-  strcpy(v3.x, "7.5369");
-  strcpy(v3.y, "4.7107");
-
-  v4.verticleNumber = 3;
-  strcpy(v4.x, "3.5559");
-  strcpy(v4.y, "2.0257");
- 
   // check qty of verticies (stored in address 0)
   EEPROM_readAnything(0, qtyVerticles); 
 
-  VerticleEEPROM verticlesArray[qtyVerticles];
-  
-    for(int i = 0; i<qtyVerticles; i++ ){
-      EEPROM_readAnything(2+i*eepromVerticleSize, verticlesArray[i]);
-    }
-
-    Serial.println(verticlesArray[0].verticleNumber);
-    Serial.println(verticlesArray[0].x);
-    Serial.println(verticlesArray[0].y);
-
-
- //read from eeprom and reconstruct objects of v.
-  
-//    EEPROM_writeAnything(2 , v1);
-//    EEPROM_writeAnything(2+eepromVerticleSize, v2);
-//    EEPROM_writeAnything(2+eepromVerticleSize*2, v3);
-//    EEPROM_writeAnything(2+eepromVerticleSize*3, v4);
-
-//  Serial.print("Size of struct ");
-//  Serial.println(sizeof(VerticleEEPROM));
-//
-//  EEPROM_readAnything(0, qtyVerticles);
-//  Serial.println(qtyVerticles);
-//  
-//  EEPROM_readAnything(2, v1);
-//  Serial.println(v1.verticleNumber);
-//  Serial.println(v1.x);
-//  Serial.println(v1.y);
-//  EEPROM_readAnything(2+eepromVerticleSize, v2);
-//  
-//  Serial.println(v2.verticleNumber);
-//  Serial.println(v2.x);
-//  Serial.println(v2.y);
-//  
-//  EEPROM_readAnything(2+eepromVerticleSize*2, v3);
-//  Serial.println(v3.verticleNumber);
-//  Serial.println(v3.x);
-//  Serial.println(v3.y);
-//
-//  EEPROM_readAnything(2+eepromVerticleSize*3, v3);
-//  Serial.println(v4.verticleNumber);
-//  Serial.println(v4.x);
-//  Serial.println(v4.y);
-
-//    EEPROM_readAnything(1+1*eepromVerticleSize, v4);
-//  EEPROM_readAnything(1+eepromVerticleSize*3, v4);
-//  Serial.println(v4.verticleNumber);
-
+  verticlesArray = (VerticleEEPROM *) malloc( qtyVerticles * sizeof(VerticleEEPROM) ); 
+ 
+  //get data from EEPROM  
+  for(int i = 0; i<qtyVerticles; i++ ){
+    EEPROM_readAnything(sizeof(qtyVerticles)+i*eepromVerticleSize, verticlesArray[i]);
+  }
 
   // starting transimission on nRF24L01
   radio.begin();
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
   radio.stopListening();
+  
 }
 void loop() {
-  //char text[] = "9,6,1,1.09038,1.02346";
-  //radio.write(&text, sizeof(text));
-  char text2[] = "9,6,2,7.09038,5.02346";
-  radio.write(&text2, sizeof(text2));
-  delay(1000);
-  
- 
+  int msgNumber = counter % qtyVerticles ;
 
+  //creating a msg
+  String msg = String(deviceID)+","+String(1)+","+String(qtyVerticles)+","+String(verticlesArray[msgNumber].verticleNumber)+
+  ","+String(verticlesArray[msgNumber].x)+","+String(verticlesArray[msgNumber].y);       
+  
+  //converting String to char Array
+  msg.toCharArray(charBuffer, 32); 
+     
+  radio.write(&charBuffer, sizeof(charBuffer));
+  //counter of the loop
+  counter++;
+  delay(1000);
 }
